8b5c1423f824f168731d70de4593e21bb8709b0f
Murphy Hsieh <murphyhsieh@leandev.io>
2019-08-27 10:05:11 +0800

    add the http package and fetch the sample api + decode it

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../services/location.dart';
 
@@ -18,8 +20,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+
+    final http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    int conditionNumber;
+    double temperature;
+    String cityName;
+
+    if (response.statusCode == 200) {
+      /// json.decode(source) == jsonDecode(source)
+
+      final decodedData = jsonDecode(response.body);
+
+      conditionNumber = decodedData['weather'][0]['id'];
+      temperature = decodedData['main']['temp'];
+      cityName = decodedData['name'];
+
+      print(conditionNumber);
+      print(temperature);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+
+//    print(location.latitude);
+//    print(location.longitude);
   }
 
   @override
